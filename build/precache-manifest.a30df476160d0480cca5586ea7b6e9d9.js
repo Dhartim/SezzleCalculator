72403b87bcf7f36a487e5c4c7a6f345ffc92c4f7
Dharti Madeka <dpmadeka@dons.usfca.edu>
2020-10-25 23:49:43 -0700

    creating react app on github

---
+++ build/precache-manifest.a30df476160d0480cca5586ea7b6e9d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1041e35e6aa3b6ad64d37262902caba",
+    "url": "/SezzleCalculator/index.html"
+  },
+  {
+    "revision": "7babbb50025d1df3346e",
+    "url": "/SezzleCalculator/static/css/main.8646196c.chunk.css"
+  },
+  {
+    "revision": "5153402e6b3598f5177f",
+    "url": "/SezzleCalculator/static/js/2.982a108e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/SezzleCalculator/static/js/2.982a108e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7babbb50025d1df3346e",
+    "url": "/SezzleCalculator/static/js/main.dd845dad.chunk.js"
+  },
+  {
+    "revision": "00cc48c109fee7b500de",
+    "url": "/SezzleCalculator/static/js/runtime-main.11a1d740.js"
+  }
+]);
