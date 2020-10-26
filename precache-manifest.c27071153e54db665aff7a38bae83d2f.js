152794c750213346e8e12d7e495c8fcac20716fa
Dharti Madeka <dpmadeka@dons.usfca.edu>
2020-10-25 22:49:30 -0700

    Updates

---
+++ precache-manifest.c27071153e54db665aff7a38bae83d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1d16852e818b5b606a1f835cb05c1b",
+    "url": "/Dhartim/SezzleCalculator/index.html"
+  },
+  {
+    "revision": "f2c0bf7ee3587466919d",
+    "url": "/Dhartim/SezzleCalculator/static/css/main.8646196c.chunk.css"
+  },
+  {
+    "revision": "5153402e6b3598f5177f",
+    "url": "/Dhartim/SezzleCalculator/static/js/2.982a108e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Dhartim/SezzleCalculator/static/js/2.982a108e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c0bf7ee3587466919d",
+    "url": "/Dhartim/SezzleCalculator/static/js/main.9aaa21c1.chunk.js"
+  },
+  {
+    "revision": "4d05a03a449b6c0df20d",
+    "url": "/Dhartim/SezzleCalculator/static/js/runtime-main.04f7b19e.js"
+  }
+]);
