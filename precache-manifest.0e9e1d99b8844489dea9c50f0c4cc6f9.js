4504052511c131a4784e682343eb4a69cba85e3b
Dharti Madeka <dpmadeka@dons.usfca.edu>
2020-10-26 15:01:49 -0700

    Updates

--- precache-manifest.0e9e1d99b8844489dea9c50f0c4cc6f9.js
+++ precache-manifest.0e9e1d99b8844489dea9c50f0c4cc6f9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5d5ae53b6f8f3716177dda14035d2456",
+    "revision": "6653a99d8737c397997d037f599ba2af",
     "url": "/SezzleCalculator/index.html"
   },
   {
-    "revision": "4b7545df86f54f770a33",
-    "url": "/SezzleCalculator/static/css/main.dada247a.chunk.css"
+    "revision": "2387402f783cd4dbb643",
+    "url": "/SezzleCalculator/static/css/main.5a34342e.chunk.css"
   },
   {
     "revision": "5153402e6b3598f5177f",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/SezzleCalculator/static/js/2.982a108e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4b7545df86f54f770a33",
-    "url": "/SezzleCalculator/static/js/main.e4a42542.chunk.js"
+    "revision": "2387402f783cd4dbb643",
+    "url": "/SezzleCalculator/static/js/main.0353fa33.chunk.js"
   },
   {
     "revision": "00cc48c109fee7b500de",
